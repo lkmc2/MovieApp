72791c7b48b003b2a01ce40e581110576229a765
lkmc2 <lkmc2@163.com>
2017-10-30 10:41:48 +0800

    add TabNavigator

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import {
     StackNavigator,
+    TabNavigator,
 } from 'react-navigation';
 
 import List from './src/pages/List.js';
 import Detail from './src/pages/Detail.js';
+import Cinemas from './src/pages/Cinemas.js';
+import MyMovies from './src/pages/MyMovies.js';
 
-const App = StackNavigator({ //利用导航栏生成界面
+const MyTab = TabNavigator({ //利用导航底栏生成界面
     List: {screen: List}, //电影列表页
+    Cinemas: {screen: Cinemas}, //影院页
+    MyMovies: {screen: MyMovies}, //我的电影页
+}, {
+
+});
+
+const App = StackNavigator({ //利用标题导航栏生成界面
+    Home: {screen: MyTab}, //底栏生成的界面
     Detail: {screen: Detail}, //详情页
 }, {
   headerMode: 'screen', //标题栏模式，内容连同标题栏一起改变
